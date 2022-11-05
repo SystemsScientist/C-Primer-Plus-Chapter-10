5c5dc33bc811ac40a238028628577e305175c693
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-05 05:14:26 -0500

    Adding Listing-10-1

---
+++ Listing-10-1/day_mon1.c
@@ -0,0 +1,18 @@
+// day_mon1.c -- prints the days for each month
+// Note: Both format specifiers should be %2d to
+//       match the book's output
+
+#include <stdio.h>
+
+#define MONTHS 12
+
+int main(void) {
+
+	int days[MONTHS] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int index;
+
+	for (index = 0; index < MONTHS; index++)
+		printf("Month %2d has %2d days.\n", index + 1, days[index]);
+
+	return 0;
+}
