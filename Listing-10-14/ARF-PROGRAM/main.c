292d4a715921d51f3c03798678280f5c262cffe0
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-10 05:43:17 -0600

    Updating directory of files

--- Listing-10-14/ARF-PROGRAM/main.c
+++ Listing-10-14/ARF-PROGRAM/main.c
@@ -1,5 +1,5 @@
 // main.c
-// compile main.c with show_array.c and mult_array.c
+// compile and execute main.c with show_array.c and mult_array.c
 // by using the following command-line scripts:
 // $ gcc main.c show_array.c mult_array.c - o main
 // $ ./main
@@ -11,11 +11,12 @@ int main(void) {
 
 	double dip[SIZE] = {20.0, 17.66, 8.2, 15.3, 22.22};
 
-	printf("The original dip array: \n");
+	printf("\nThe original dip array: \n");
 	show_array(dip, SIZE);
 	mult_array(dip, SIZE, 2.5);
 	printf("The dip array after calling mult_array(): \n");
 	show_array(dip, SIZE);
-
+	
+	printf("\n");
 	return 0;
 }
